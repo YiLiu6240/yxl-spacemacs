da57475a832df2ac2a8a0fc6639e8bac5142681b
YiLiu6240 <yi.liu6240@gmail.com>
2016-10-05 20:22:24 +0100

    workspace tweak

--- layers/yxl-workspace/packages.el
+++ layers/yxl-workspace/packages.el
@@ -5,13 +5,72 @@
     (add-to-list 'window-persistent-parameters '(window-side . writable))
     (add-to-list 'window-persistent-parameters '(window-slot . writable))
     (setq eyebrowse-new-workspace 'spacemacs/home)
+    (defun spacemacs//workspaces-mod-ts-hint ()
+      "Return a one liner string containing all the workspaces names."
+      (concat
+       " "
+       (mapconcat 'spacemacs//workspace-format-name
+                  (eyebrowse--get 'window-configs) " | ")
+       (if (equal 1 spacemacs--ts-full-hint-toggle)
+           spacemacs--workspaces-mod-ts-full-hint
+         (concat "  (["
+                 (propertize "?" 'face 'hydra-face-red)
+                 "] help)"))))
+    (spacemacs|transient-state-format-hint workspaces-mod
+      spacemacs--workspaces-mod-ts-full-hint
+      "\n\n
+ Go to^^^^^^                         Actions^^
+ ─────^^^^^^───────────────────────  ───────^^──────────────────────
+ [_0_,_9_]^^     nth/new workspace   [_d_] close current workspace
+ [_C-0_,_C-9_]^^ nth/new workspace   [_R_] rename current workspace
+ [_<tab>_]^^^^   last workspace      [_?_] toggle help
+ [_w_] select window config          [_C-c_] create window config
+ [_n_/_C-l_]^^   next workspace
+ [_N_/_p_/_C-h_] prev workspace\n")
+    (spacemacs|define-transient-state workspaces-mod
+      :title "Workspaces Transient State"
+      :hint-is-doc t
+      :dynamic-hint (spacemacs//workspaces-mod-ts-hint)
+      :bindings
+      ("?" spacemacs//workspaces-ts-toggle-hint)
+      ("0" eyebrowse-switch-to-window-config-0 :exit t)
+      ("1" eyebrowse-switch-to-window-config-1 :exit t)
+      ("2" eyebrowse-switch-to-window-config-2 :exit t)
+      ("3" eyebrowse-switch-to-window-config-3 :exit t)
+      ("4" eyebrowse-switch-to-window-config-4 :exit t)
+      ("5" eyebrowse-switch-to-window-config-5 :exit t)
+      ("6" eyebrowse-switch-to-window-config-6 :exit t)
+      ("7" eyebrowse-switch-to-window-config-7 :exit t)
+      ("8" eyebrowse-switch-to-window-config-8 :exit t)
+      ("9" eyebrowse-switch-to-window-config-9 :exit t)
+      ("C-0" eyebrowse-switch-to-window-config-0)
+      ("C-1" eyebrowse-switch-to-window-config-1)
+      ("C-2" eyebrowse-switch-to-window-config-2)
+      ("C-3" eyebrowse-switch-to-window-config-3)
+      ("C-4" eyebrowse-switch-to-window-config-4)
+      ("C-5" eyebrowse-switch-to-window-config-5)
+      ("C-6" eyebrowse-switch-to-window-config-6)
+      ("C-7" eyebrowse-switch-to-window-config-7)
+      ("C-8" eyebrowse-switch-to-window-config-8)
+      ("C-9" eyebrowse-switch-to-window-config-9)
+      ("<tab>" eyebrowse-last-window-config)
+      ("C-h" eyebrowse-prev-window-config)
+      ("C-i" eyebrowse-last-window-config)
+      ("C-l" eyebrowse-next-window-config)
+      ("C-c" eyebrowse-create-window-config)
+      ("d" eyebrowse-close-window-config)
+      ("n" eyebrowse-next-window-config)
+      ("N" eyebrowse-prev-window-config)
+      ("p" eyebrowse-prev-window-config)
+      ("R" spacemacs/workspaces-ts-rename :exit t)
+      ("w" eyebrowse-switch-to-window-config))
     ;; cut integration between persp and workspaces
     (remove-hook 'persp-before-switch-functions
-              #'spacemacs/update-eyebrowse-for-perspective)
+                 #'spacemacs/update-eyebrowse-for-perspective)
     (remove-hook 'eyebrowse-post-window-switch-hook
-              #'spacemacs/save-eyebrowse-for-perspective)
+                 #'spacemacs/save-eyebrowse-for-perspective)
     (remove-hook 'persp-activated-functions
-              #'spacemacs/load-eyebrowse-for-perspective)
+                 #'spacemacs/load-eyebrowse-for-perspective)
     (remove-hook 'persp-before-save-state-to-file-functions
                  #'spacemacs/update-eyebrowse-for-perspective)
     (remove-hook 'persp-after-load-state-functions
