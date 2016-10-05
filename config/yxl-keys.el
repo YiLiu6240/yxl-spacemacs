0db045af10b83571e99112419dcfa39af4a9dfd5
YiLiu6240 <yi.liu6240@gmail.com>
2016-10-05 10:18:06 +0100

    bindings

--- config/yxl-keys.el
+++ config/yxl-keys.el
@@ -1,16 +1,19 @@
+;; mac: use super+3 as "#"
 (global-set-key (kbd "s-3") (lambda () (interactive) (insert "#")))
 ;; (global-set-key (kbd "C-S-p") #'helm-M-x)
 (global-set-key (kbd "C-S-p") #'counsel-M-x)
 (global-set-key (kbd "C-h") #'delete-backward-char)
-; REVIEW: check if C-h still works in ex mode
-;; (define-key minibuffer-local-map (kbd "C-h") #'backward-delete-char)
 (define-key isearch-mode-map "\C-h" #'isearch-delete-char)
 
 
+;; behaviour changes to stock bindings
 (spacemacs/set-leader-keys
-  ;; spacemacs stock
   "<SPC>" #'evil-avy-goto-char-2
   "ws" #'split-window-below-and-focus
   "wS" #'split-window-below
   "wv" #'split-window-right-and-focus
   "wV" #'split-window-right)
+
+;; addition to stock bindings
+(spacemacs/set-leader-keys
+  "hdF" #'describe-face)
