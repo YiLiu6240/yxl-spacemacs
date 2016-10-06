7cb16fffa778d00fe62b446d139346fc27442dfb
YiLiu6240 <yi.liu6240@gmail.com>
2016-10-06 21:51:38 +0100

    python bug fixes

--- layers/yxl-prog/packages.el
+++ layers/yxl-prog/packages.el
@@ -26,6 +26,25 @@
 (defun yxl-prog/post-init-python ()
   (with-eval-after-load 'python
     (add-hook 'python-mode-hook 'which-function-mode)
+    (defun python-start-or-switch-repl ()
+      "Start and/or switch to the REPL."
+      (interactive)
+      (let ((shell-process
+             (or (python-shell-get-process)
+                 ;; `run-python' has different return values and different
+                 ;; errors in different emacs versions. In 24.4, it throws an
+                 ;; error when the process didn't start, but in 25.1 it
+                 ;; doesn't throw an error, so we demote errors here and
+                 ;; check the process later
+                 (with-demoted-errors "Error: %S"
+                   ;; in Emacs 24.5 and 24.4, `run-python' doesn't return the
+                   ;; shell process
+                   (call-interactively #'run-python)
+                   (python-shell-get-process)))))
+        (unless shell-process
+          (error "Failed to start python shell properly"))
+        (pop-to-buffer (process-buffer shell-process))
+        (evil-insert-state)))
     ;; (add-hook 'python-mode-hook 'evil-visual-mark-mode)
     ;; (add-hook 'python-mode-hook (lambda () (modify-syntax-entry ?_ "w")))
     )
