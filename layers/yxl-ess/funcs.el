be9c8ce08e570eb7c9937c0023b7a93919f3ac3b
YiLiu6240 <yi.liu6240@gmail.com>
2016-10-08 19:05:54 +0100

    ess config
    refactor

--- layers/yxl-ess/funcs.el
+++ layers/yxl-ess/funcs.el
@@ -1,56 +1,56 @@
-  (defun yxl-ess/ess-repl-2cols ()
-    (interactive)
-    (delete-other-windows)
+(defun yxl-ess/ess-repl-2cols ()
+  (interactive)
+  (delete-other-windows)
 
-    ;; create two cols
-    (split-window-right)
+  ;; create two cols
+  (split-window-right)
 
-    ;; create an empty R mode buffer
-    (split-window-below -15)
-    (windmove-down)
-    (let ((newbuf (generate-new-buffer-name "untitled")))
-      (switch-to-buffer newbuf)
-      (R-mode))
-    (windmove-up)
+  ;; create an empty R mode buffer
+  (split-window-below -15)
+  (windmove-down)
+  (let ((newbuf (generate-new-buffer-name "untitled")))
+    (switch-to-buffer newbuf)
+    (R-mode))
+  (windmove-up)
 
-    ;; create ess process and start a helper buffer
-    (windmove-right)
-    (split-window-below 20)
-    (windmove-down)
-    (call-interactively 'R)
-    (split-window-below -20)
-    (ess-help "ls")
+  ;; create ess process and start a helper buffer
+  (windmove-right)
+  (split-window-below 20)
+  (windmove-down)
+  (call-interactively 'R)
+  (split-window-below -20)
+  (ess-help "ls")
 
-    ;; restore focus to main window
-    (windmove-left)
-    (windmove-up))
+  ;; restore focus to main window
+  (windmove-left)
+  (windmove-up))
 
-  (defun yxl-ess/ess-repl-3cols ()
-    (interactive)
-    (delete-other-windows)
+(defun yxl-ess/ess-repl-3cols ()
+  (interactive)
+  (delete-other-windows)
 
-    ;; create three cols
-    (split-window-right)
-    (split-window-right)
+  ;; create three cols
+  (split-window-right)
+  (split-window-right)
 
-    ;; create an empty R mode buffer
-    (split-window-below -20)
-    (windmove-down)
-    (let ((newbuf (generate-new-buffer-name "untitled")))
-      (switch-to-buffer newbuf)
-      (R-mode))
-    (windmove-up)
+  ;; create an empty R mode buffer
+  (split-window-below -20)
+  (windmove-down)
+  (let ((newbuf (generate-new-buffer-name "untitled")))
+    (switch-to-buffer newbuf)
+    (R-mode))
+  (windmove-up)
 
-    ;; create a middle pane
-    (windmove-right)
-    (split-window-below -20)
+  ;; create a middle pane
+  (windmove-right)
+  (split-window-below -20)
 
-    ;; create ess process and start a helper buffer
-    (windmove-right)
-    (call-interactively 'R)
-    (split-window-below -20)
-    (ess-help "ls")
+  ;; create ess process and start a helper buffer
+  (windmove-right)
+  (call-interactively 'R)
+  (split-window-below -20)
+  (ess-help "ls")
 
-    ;; restore focus to main window
-    (windmove-left)
-    (windmove-left))
+  ;; restore focus to main window
+  (windmove-left)
+  (windmove-left))
--- layers/yxl-ess/packages.el
+++ layers/yxl-ess/packages.el
@@ -1,73 +1,76 @@
 (setq yxl-ess-packages '(ess))
 
+(defun yxl-ess/yxl-ess-settings ()
+  "wrap my settings in a function"
+  (ess-set-style 'RRR 'quiet)
+  ;; disable "<-" assignment when pressing "_" --------
+  ;; TODO: make sure
+  (setq ess-S-assign "_")
+  ;; --------
+  (setq ess-indent-offset 4)
+  ;; no fancy comment
+  (setq ess-fancy-comments nil
+        comment-add 0)
+  ;; no history file
+  (setq ess-history-file nil)
+  ;; no spaces around argument assignment
+  (setq ess-R-argument-suffix "=")
+  ;; speed ess eval efficiency
+  (setq ess-eval-visibly 'nowait)
+  ;; show results in buffer when calling ess-execute
+  (setq ess-execute-in-process-buffer t)
+  ;; Start R in the working directory by default
+  (setq ess-ask-for-ess-directory nil))
+
+(defun yxl-ess/yxl-ess-hook ()
+  (ess-toggle-S-assign nil)
+  (setq evil-shift-width 4))
+
 (defun yxl-ess/post-init-ess ()
   ;; personal preferences
   (with-eval-after-load 'ess-mode
-    (defun yxl-ess/yxl-ess-settings ()
-      "wrap my settings in a function"
-      (ess-set-style 'RRR 'quiet)
-      ;; disable "<-" assignment when pressing "_" --------
-      ;; TODO: make sure
-      (setq ess-S-assign "_")
-      (ess-toggle-S-assign nil)
-      ;; --------
-      (setq ess-indent-offset 4)
-      ;; no fancy comment
-      (setq ess-fancy-comments nil
-            comment-add 0)
-      ;; no history file
-      (setq ess-history-file nil)
-      ;; no spaces around argument assignment
-      (setq ess-R-argument-suffix "=")
-      ;; speed ess eval efficiency
-      (setq ess-eval-visibly 'nowait)
-      ;; show results in buffer when calling ess-execute
-      (setq ess-execute-in-process-buffer t)
-      ;; Start R in the working directory by default
-      (setq ess-ask-for-ess-directory nil))
-
-    (add-hook 'ess-mode-hook 'yxl-ess/yxl-ess-settings))
+    (yxl-ess/yxl-ess-settings)
+    (add-hook 'ess-mode-hook #'yxl-ess/yxl-ess-hook))
 
   ;; ess main configs
-  (with-eval-after-load 'ess-mode
-    ;; ess hooks
-    (add-hook 'ess-mode-hook 'smartparens-mode)
-    (add-hook 'ess-mode-hook 'fci-mode)
-    (add-hook 'ess-mode-hook 'hl-todo-mode)
-    (add-hook 'ess-mode-hook 'which-function-mode)
-    ;; (add-hook 'ess-mode-hook 'evil-visual-mark-mode)
+  ;; ess hooks
+  (add-hook 'ess-mode-hook 'smartparens-mode)
+  (add-hook 'ess-mode-hook 'fci-mode)
+  (add-hook 'ess-mode-hook 'hl-todo-mode)
+  (add-hook 'ess-mode-hook 'which-function-mode)
+  ;; (add-hook 'ess-mode-hook 'evil-visual-mark-mode)
 
-    ;; flycheck lintr
-    (setq flycheck-lintr-linters
-          (concat "with_defaults(assignment_linter=NULL, "
-                  "camel_case_linter=NULL, "
-                  "commented_code_linter=NULL, "
-                  "infix_spaces_linter=NULL)"))
+  ;; flycheck lintr
+  (setq flycheck-lintr-linters
+        (concat "with_defaults(assignment_linter=NULL, "
+                "camel_case_linter=NULL, "
+                "commented_code_linter=NULL, "
+                "infix_spaces_linter=NULL)"))
 
-    ;; ess self functions
-    (defun yxl/ess-lsos ()
-      "invoke lsos function, this function needs to be in environment"
-      (interactive)
-      (ess-execute "lsos()"))
-    (defun yxl/ess-lsdf ()
-      "invoke lsos function, this function needs to be in environment"
-      (interactive)
-      (ess-execute "lsdf()"))
+  ;; ess self functions
+  (defun yxl/ess-lsos ()
+    "invoke lsos function, this function needs to be in environment"
+    (interactive)
+    (ess-execute "lsos()"))
+  (defun yxl/ess-lsdf ()
+    "invoke lsos function, this function needs to be in environment"
+    (interactive)
+    (ess-execute "lsdf()"))
 
-    ;; overwrite: change process function
-    (add-hook
-     'ess-mode-hook
-     (lambda ()
-       (spacemacs/set-leader-keys-for-major-mode 'ess-mode
-         "si" 'ess-switch-to-inferior-or-script-buffer)))
+  ;; overwrite: change process function
+  (add-hook
+   'ess-mode-hook
+   (lambda ()
+     (spacemacs/set-leader-keys-for-major-mode 'ess-mode
+       "si" 'ess-switch-to-inferior-or-script-buffer)))
 
-    ;; ess mode leader key bindings
-    (spacemacs/set-leader-keys-for-major-mode 'ess-mode
-      ",e" 'ess-execute
-      ",d" 'ess-rdired
-      ",fs" 'yxl/ess-lsos
-      ",ff" 'yxl/ess-lsdf
-      ",h" 'ess-help))
+  ;; ess mode leader key bindings
+  (spacemacs/set-leader-keys-for-major-mode 'ess-mode
+    ",e" 'ess-execute
+    ",d" 'ess-rdired
+    ",fs" 'yxl/ess-lsos
+    ",ff" 'yxl/ess-lsdf
+    ",h" 'ess-help)
 
   ;; ess outline minor mode
   (with-eval-after-load 'ess-mode
