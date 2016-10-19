19d36eb128088ea06de3769ab65d80c2f44dc535
YiLiu6240 <yi.liu6240@gmail.com>
2016-10-19 12:13:34 +0100

    modeline: vim-powerline
    and remove window-numbering

--- init.el
+++ init.el
@@ -110,6 +110,7 @@ values."
    dotspacemacs-excluded-packages '(persp-mode
                                     smooth-scrolling
                                     spaceline
+                                    window-numbering
                                     ido)
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
--- layers/yxl-ui/packages.el
+++ layers/yxl-ui/packages.el
@@ -1,10 +1,50 @@
 (setq yxl-ui-packages '(spaceline
                         (yxl-spaceline :toggle (bound-and-true-p spaceline))
                         (yxl-mode-line :location local
-                                       :toggle (not (bound-and-true-p spaceline)))))
+                                       :toggle (not (bound-and-true-p spaceline)))
+                        powerline
+                        (vim-powerline :location local)))
 
-(defun yxl-ui/init-yxl-mode-line ()
-  (use-package yxl-mode-line))
+;; (defun yxl-ui/init-yxl-mode-line ()
+;;   (use-package yxl-mode-line))
 
-(defun yxl-ui/init-yxl-spaceline ()
-  (use-package yxl-spaceline))
+;; (defun yxl-ui/init-yxl-spaceline ()
+;;   (use-package yxl-spaceline))
+(defun yxl-ui/init-powerline ())
+
+(defun yxl-ui/init-vim-powerline ()
+  (require 'powerline)
+  ;; (if (display-graphic-p)
+  ;;     (setq powerline-default-separator 'arrow)
+  ;;   (setq powerline-default-separator 'utf-8))
+  (setq powerline-default-separator nil)
+
+  (defun powerline-raw (str &optional face pad)
+  "Render STR as mode-line data using FACE and optionally
+PAD import on left (l) or right (r) or left-right (lr)."
+  (when str
+    (let* ((rendered-str (format-mode-line str))
+           (padded-str (concat
+                        (when (and (> (length rendered-str) 0)
+                                   (or (eq pad 'l) (eq pad 'lr))) " ")
+                        (if (listp str) rendered-str str)
+                        (when (and (> (length rendered-str) 0)
+                                   (or (eq pad 'r) (eq pad 'lr))) " "))))
+
+      (if face
+          (pl/add-text-property padded-str 'face face)
+        padded-str))))
+
+  (require 'vim-powerline-theme)
+  (powerline-vimish-theme)
+
+  (defun spacemacs//set-vimish-powerline-for-startup-buffers ()
+    "Set the powerline for buffers created when Emacs starts."
+    (dolist (buffer '("*Messages*" "*spacemacs*" "*Compile-Log*"))
+      (when (get-buffer buffer)
+        (with-current-buffer buffer
+          (setq-local mode-line-format (default-value 'mode-line-format))
+          (powerline-set-selected-window)
+          (powerline-reset)))))
+  (add-hook 'emacs-startup-hook
+            'spacemacs//set-vimish-powerline-for-startup-buffers))
