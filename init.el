1d50fddfee95f9043ca1bf68a0d40b3164e2e296
YiLiu6240 <yi.liu6240@gmail.com>
2016-10-27 22:54:07 +0100

    tidy up

--- init.el
+++ init.el
@@ -338,15 +338,15 @@ values."
   ;; --------
   (with-eval-after-load 'flycheck
     (flycheck-define-checker proselint
-                             "A linter for prose."
-                             :command ("proselint" source-inplace)
-                             :error-patterns
-                             ((warning line-start (file-name) ":" line ":" column ": "
-                                       (id (one-or-more (not (any " "))))
-                                       (message (one-or-more not-newline)
-                                                (zero-or-more "\n" (any " ") (one-or-more not-newline)))
-                                       line-end))
-                             :modes (text-mode markdown-mode gfm-mode LaTeX-mode))
+      "A linter for prose."
+      :command ("proselint" source-inplace)
+      :error-patterns
+      ((warning line-start (file-name) ":" line ":" column ": "
+                (id (one-or-more (not (any " "))))
+                (message (one-or-more not-newline)
+                         (zero-or-more "\n" (any " ") (one-or-more not-newline)))
+                line-end))
+      :modes (text-mode markdown-mode gfm-mode LaTeX-mode))
     (add-to-list 'flycheck-checkers 'proselint))
 
   ;; --------
