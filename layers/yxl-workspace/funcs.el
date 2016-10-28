8c93c9afef0984a8c09f4aee89ed3f574c692a1e
YiLiu6240 <yi.liu6240@gmail.com>
2016-10-28 17:43:50 +0100

    use range format

--- layers/yxl-workspace/funcs.el
+++ layers/yxl-workspace/funcs.el
@@ -90,7 +90,7 @@
   "remove existing configs and
 pre-populate/re-populate fake configs with names."
   (interactive)
-  (mapcar 'eyebrowse--delete-window-config '(0 1 2 3 4 5 6 7 8 9))
+  (mapcar 'eyebrowse--delete-window-config '(number-sequence 0 9))
   (eyebrowse--insert-in-window-config-list
    (eyebrowse--current-window-config 0 "build"))
   (eyebrowse--insert-in-window-config-list
