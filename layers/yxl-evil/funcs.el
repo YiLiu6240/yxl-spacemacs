d144ca8265c82734f7aa7942199320d260c8350b
YiLiu6240 <yi.liu6240@gmail.com>
2016-10-28 16:30:01 +0100

    bug fixes

--- layers/yxl-evil/funcs.el
+++ layers/yxl-evil/funcs.el
@@ -76,10 +76,12 @@
     (define-key evil-motion-state-map (kbd "C-a j") #'yxl-frame/select-repl)
     (define-key evil-motion-state-map (kbd "C-a k") #'yxl-frame/select-code)
     (define-key evil-motion-state-map (kbd "C-a l") #'yxl-frame/select-config)
-    (define-key evil-evilified-state-map (kbd "C-a h") #'yxl-frame/select-meta)
-    (define-key evil-evilified-state-map (kbd "C-a j") #'yxl-frame/select-repl)
-    (define-key evil-evilified-state-map (kbd "C-a k") #'yxl-frame/select-code)
-    (define-key evil-evilified-state-map (kbd "C-a l") #'yxl-frame/select-config)))
+    (when (boundp 'evil-evilified-state-map-original)
+      (progn
+        (define-key evil-evilified-state-map-original (kbd "C-a h") #'yxl-frame/select-meta)
+        (define-key evil-evilified-state-map-original (kbd "C-a j") #'yxl-frame/select-repl)
+        (define-key evil-evilified-state-map-original (kbd "C-a k") #'yxl-frame/select-code)
+        (define-key evil-evilified-state-map-original (kbd "C-a l") #'yxl-frame/select-config)))))
 
 (defun yxl-evil/setup-evil-misc ()
   (evil-define-key 'insert comint-mode-map
