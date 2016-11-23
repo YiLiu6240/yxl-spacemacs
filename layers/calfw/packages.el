9f199f8db4bf06339795b01de7c0b80dd65b8bf8
YiLiu6240 <yi.liu6240@gmail.com>
2016-11-23 15:44:41 +0000

    tweak calfw

--- layers/calfw/packages.el
+++ layers/calfw/packages.el
@@ -10,22 +10,18 @@ which require an initialization must be listed explicitly in the list.")
   (use-package calfw
     :defer t
     :commands (cfw-open-calendar)
-    :init
-    (progn
-      (spacemacs/set-leader-keys
-        "aK" 'cfw-open-calendar)
-      (evil-set-initial-state 'cfw:calendar-mode 'evilified))
     :config
     (progn
-     (require 'calfw-cal)
-     (require 'calfw-ical)
-     (require 'calfw-org)
+      (evil-set-initial-state 'cfw:calendar-mode 'evilified)
+      (require 'calfw-cal)
+      (require 'calfw-ical)
+      (require 'calfw-org)
 
-     (defun cfw-open-calendar()
-       (interactive)
-       (cfw:open-calendar-buffer
-        :contents-sources
-        (list
-         (cfw:org-create-source "#268bd2"))))
+      (defun cfw-open-calendar()
+        (interactive)
+        (cfw:open-calendar-buffer
+         :contents-sources
+         (list
+          (cfw:org-create-source "#268bd2"))))
 
-     (calendar-set-date-style 'iso))))
+      (calendar-set-date-style 'iso))))
