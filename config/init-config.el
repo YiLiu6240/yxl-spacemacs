44855306b272c4ada0992407e915dad0e4985f5e
yxl105_X51 <yi.liu6240@gmail.com>
2016-11-25 10:39:11 +0000

    remove frame size tweak

--- config/init-config.el
+++ config/init-config.el
@@ -1,7 +1,7 @@
 ;; project stuff
 (defvar-local master-dir nil)
 
-(push '(width . 84) initial-frame-alist)
-(push '(height . 60) initial-frame-alist)
-(push '(width . 84) default-frame-alist)
-(push '(height . 60) default-frame-alist)
+;; (push '(width . 84) initial-frame-alist)
+;; (push '(height . 60) initial-frame-alist)
+;; (push '(width . 84) default-frame-alist)
+;; (push '(height . 60) default-frame-alist)
