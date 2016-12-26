436821329a8cecf13372cbfd0b60101a78441076
YiLiu6240 <yi.liu6240@gmail.com>
2016-12-26 17:56:40 +0000

    add fallback options for yxl-helm-quick

--- layers/yxl/local/yxl-helm-quick/yxl-helm-quick.el
+++ layers/yxl/local/yxl-helm-quick/yxl-helm-quick.el
@@ -2,34 +2,56 @@
 
 
 
+(setq yxl-helm-quick--fallback
+      (helm-build-sync-source "Helm Quick"
+        :match (lambda (_candidate) t)  ;; persistent
+        :candidates '(("yxl-helm-quick" . yxl-helm-quick))
+        :action (lambda (candidate) (funcall candidate))))
+
 (defun yxl-helm-org-files ()
   (interactive)
-  (helm :sources '((name . "Org agenda")
+  (helm :sources '(((name . "Org agenda")
                    (candidates . yxl-org-files)
                    (action . (("open" . find-file)
                               ("open other window" . find-file-other-window))))
+                   yxl-helm-quick--fallback)
         :buffer "*helm org agenda*"))
 
 (defun yxl-helm-set-simple-todo ()
   (interactive)
-  (helm :sources '((name . "Simple todo")
+  (helm :sources '(((name . "Simple todo")
                    (candidates . (("task1" . yxl-set-simple-todo-task1)
                                   ("task2" . yxl-set-simple-todo-task2)
                                   ("task3" . yxl-set-simple-todo-task3)))
                    (action . (("open" . (lambda (x) (call-interactively x))))))
+                   yxl-helm-quick--fallback)
         :buffer "*helm yxl simple-todo*"))
 
 (defun yxl-helm-websites ()
   (interactive)
-  (helm :sources '((name . "Websites")
+  (helm :sources '(((name . "Websites")
                    (candidates . yxl-env-files-alist)
-                   (action . (("open" . (lambda (x) (browse-url-generic x))))))))
+                   (action . (("open" . (lambda (x) (browse-url-generic x))))))
+                   yxl-helm-websites--fallback)))
+
+(setq yxl-helm-websites--fallback
+      (helm-build-sync-source "Fallback"
+        :match (lambda (_candidate) t)
+        :candidates '(("yxl-helm-quick" . (lambda (x) (yxl-helm-quick)))
+                      ("Google search" . (lambda (x)
+                                           (let* ((google-base "http://www.google.com/search?q=%s")
+                                                  (query-string (replace-regexp-in-string " " "\+" x))
+                                                  (url-string (format google-base query-string)))
+                                             (browse-url-generic url-string))))
+                      ("Direct Input" . (lambda (x) (browse-url-generic x))))
+        :action (lambda (candidate) (funcall candidate helm-pattern))))
 
 (defun yxl-helm-files ()
   (interactive)
-  (helm :sources '((name . "Files and Directories")
+  (helm :sources '(((name . "Files and Directories")
                    (candidates . yxl-env-websites-alist)
-                   (action . (("open" . (lambda (x) (find-file x))))))))
+                   (action . (("open" . (lambda (x) (find-file x))))))
+                   yxl-helm-quick--fallback)))
 
 (defun yxl-helm-quick ()
   (interactive)
