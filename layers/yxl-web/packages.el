021a9821694458c57c37d280a1b6b5b996681a64
YiLiu6240 <yi.liu6240@gmail.com>
2017-01-01 14:05:34 +0000

    add date column in elfeed; elfeed powerline tweak

--- layers/yxl-web/packages.el
+++ layers/yxl-web/packages.el
@@ -138,14 +138,14 @@
     (add-hook 'elfeed-show-mode-hook #'yxl-web/elfeed-show-mode-config)
     (defalias 'elfeed-toggle-star
       (elfeed-expose #'elfeed-search-toggle-all 'star))
-
+    (setq elfeed-goodies/powerline-default-separator 'nil)
     (yxl-web/elfeed-bindings)
     (yxl-web/elfeed-hydra-setup)
+    (yxl-web/elfeed-patch)
 
     (defadvice elfeed-show-yank (after elfeed-show-yank-to-kill-ring activate compile)
       "Insert the yanked text from x-selection to kill ring"
       (kill-new (x-get-selection)))
-
     (ad-activate 'elfeed-show-yank)))
 
 (defun yxl-web/init-atomic-chrome ()
