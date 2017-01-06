9cb10846440ae74db093695e5809c5285f338408
YiLiu6240 <yi.liu6240@gmail.com>
2017-01-06 13:36:57 +0000

    new feature: toggle w3m as the default browser

--- layers/yxl-web/packages.el
+++ layers/yxl-web/packages.el
@@ -58,6 +58,7 @@
         "aws" 'w3m-search
         "awS" 'w3m-search-new-session
         "awr" 'w3m-reddit
+        "awt" 'yxl-web/toggle-w3m
         "awh" 'w3m-hackernews))
     :config
     (progn
