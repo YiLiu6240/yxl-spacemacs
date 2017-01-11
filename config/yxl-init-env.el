ec56cf4987dd56cfb14e25139e48fbe58db3f750
YiLiu6240 <yi.liu6240@gmail.com>
2017-01-11 12:47:30 +0000

    config: set my env variables using setq, not defvar

--- config/yxl-init-env.el
+++ config/yxl-init-env.el
@@ -1,35 +1,35 @@
-(defvar yxl-path-dotfiles "~/dotfiles/")
-(defvar yxl-path-sync "~/Dropbox/")  ;; sync location for repos
-(defvar yxl-path-local "~/local-repo/")  ;; local location for repos
-(defvar yxl-path-downloads "~/Downloads/")
-(defvar yxl-path-org "~/Dropbox/org/")
-(defvar yxl-path-org-task "~/Dropbox/org/tasks/")
-(defvar yxl-path-projects (concat yxl-path-local "yxl_projects"))
+(setq yxl-path-dotfiles "~/dotfiles/")
+(setq yxl-path-sync "~/Dropbox/")  ;; sync location for repos
+(setq yxl-path-local "~/local-repo/")  ;; local location for repos
+(setq yxl-path-downloads "~/Downloads/")
+(setq yxl-path-org "~/Dropbox/org/")
+(setq yxl-path-org-task "~/Dropbox/org/tasks/")
+(setq yxl-path-projects (concat yxl-path-local "yxl_projects"))
 
-(defvar yxl-path-code-repo (concat yxl-path-local "c3_pwd/"))
-(defvar yxl-path-phd-repo (concat yxl-path-local "yxl105_ctw/"))
-(defvar yxl-path-code-pwd (concat yxl-path-local "c3_pwd/code/"))
-(defvar yxl-path-paper-pwd (concat yxl-path-local "yxl105_ctw/yxl105_tex/"))
-(defvar yxl-path-journal-pwd (concat yxl-path-sync "lit_1_yxl105/1_thesis/"))
-(defvar yxl-file-bib (concat yxl-path-sync "bib/yxl_bib_master.bib"))
+(setq yxl-path-code-repo (concat yxl-path-local "c3_pwd/"))
+(setq yxl-path-phd-repo (concat yxl-path-local "yxl105_ctw/"))
+(setq yxl-path-code-pwd (concat yxl-path-local "c3_pwd/code/"))
+(setq yxl-path-paper-pwd (concat yxl-path-local "yxl105_ctw/yxl105_tex/"))
+(setq yxl-path-journal-pwd (concat yxl-path-sync "lit_1_yxl105/1_thesis/"))
+(setq yxl-file-bib (concat yxl-path-sync "bib/yxl_bib_master.bib"))
 
 
 
-(defvar yxl-file-note-master "~/Dropbox/org/note.org")
-(defvar yxl-file-org-main (concat yxl-path-org-task "tasks_1_main.org"))
-(defvar yxl-file-org-work (concat yxl-path-org-task "tasks_2_work.org"))
-(defvar yxl-file-org-config (concat yxl-path-org-task "tasks_3_config.org"))
-(defvar yxl-file-org-proj (concat yxl-path-org-task "tasks_4_proj.org"))
-(defvar yxl-file-org-log (concat yxl-path-org "logs/" "log.org"))
-(defvar yxl-file-org-scratch (concat yxl-path-org "scratch.org"))
+(setq yxl-file-note-master "~/Dropbox/org/note.org")
+(setq yxl-file-org-main (concat yxl-path-org-task "tasks_1_main.org"))
+(setq yxl-file-org-work (concat yxl-path-org-task "tasks_2_work.org"))
+(setq yxl-file-org-config (concat yxl-path-org-task "tasks_3_config.org"))
+(setq yxl-file-org-proj (concat yxl-path-org-task "tasks_4_proj.org"))
+(setq yxl-file-org-log (concat yxl-path-org "logs/" "log.org"))
+(setq yxl-file-org-scratch (concat yxl-path-org "scratch.org"))
 
-(defvar yxl-org-task-files (list yxl-file-org-scratch
+(setq yxl-org-task-files (list yxl-file-org-scratch
                                  yxl-file-org-main
                                  yxl-file-org-work
                                  yxl-file-org-config
                                  yxl-file-org-proj))
 
-(defvar yxl-org-files (append yxl-org-task-files
+(setq yxl-org-files (append yxl-org-task-files
                               (list
                                (concat yxl-path-org-task "cal_gen.org")
                                (concat yxl-path-org-task "cal_google.org")
