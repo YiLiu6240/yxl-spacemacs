e7dd1bf00edb056fb0be8358f7d7647b80e43cec
YiLiu6240 <yi.liu6240@gmail.com>
2017-01-13 13:42:20 +0000

    config: simplify init.el
    - try to make init.el as clean as possible
    - post config setups split into two files
      - config-post-init.el: general settings in user-config stage
      - hack-post-init.el: settings that must be loaded in the end to override stuff

--- config/config-init.el
+++ config/config-init.el
@@ -5,3 +5,18 @@
 ;; (push '(height . 60) initial-frame-alist)
 ;; (push '(width . 84) default-frame-alist)
 ;; (push '(height . 60) default-frame-alist)
+
+(if (not (display-graphic-p))
+    ;; TODO: tweak various bg settings in TUI
+    (setq yxl-theme-set-bg nil))
+
+(if (eq system-type 'windows-nt)
+    (progn
+      ;; use external ls
+      ;; use `where' as equivalent of `which'
+      (setq ls-lisp-use-insert-directory-program t)
+      (setq insert-directory-program "c:/tools/msys64/usr/bin/ls")))
+(if (eq system-type 'darwin)
+    ;; osx setup of pdf-tools
+    ;; http://emacs.stackexchange.com/questions/13314/install-pdf-tools-on-emacs-macosx
+    (setq pdf-info-epdfinfo-program "/usr/local/bin/epdfinfo"))
