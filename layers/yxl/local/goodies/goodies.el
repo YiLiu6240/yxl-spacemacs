6ff3cc8d22afca5447ff17115deb9d0f5deaed1a
YiLiu6240 <yi.liu6240@gmail.com>
2017-01-15 20:53:38 +0000

    config: add funcs

--- layers/yxl/local/goodies/goodies.el
+++ layers/yxl/local/goodies/goodies.el
@@ -28,46 +28,5 @@ Useful when creating new window layout/config."
   (popwin:popup-buffer (find-file-noselect default-directory)
                        :width 40 :position 'left :stick t))
 
-(defun round-nb-in-region ()
-  "http://stackoverflow.com/questions/23636226/how-to-round-all-the-numbers-in-a-region"
-  (interactive)
-  (let ((round-format (read-string "enter format (%0.4f): ")))
-    (save-restriction
-      (narrow-to-region (region-beginning) (region-end))
-      (goto-char 1)
-      (let ((case-fold-search nil))
-        (while (search-forward-regexp "\\([0-9]+\\.[0-9]+\\)" nil t)
-          (replace-match (format round-format
-                                 (string-to-number (match-string 1)))))))))
-
-(defun copy-to-clipboard ()
-  "Copies selection to x-clipboard."
-  (interactive)
-  (let ((cli-cmd (cond ((eq system-type 'darwin) "pbcopy")
-                       ((eq system-type 'gnu/linux) "xsel --clipboard --input"))))
-   (if (display-graphic-p)
-      (progn
-        (message "Yanked region to x-clipboard!")
-        (call-interactively 'clipboard-kill-ring-save))
-    (if (region-active-p)
-        (progn
-          (shell-command-on-region (region-beginning) (region-end) cli-cmd)
-          (message "Yanked region to clipboard!")
-          (deactivate-mark))
-      (message "No region active; can't yank to clipboard!")))))
-
-(defun paste-from-clipboard ()
-  "Pastes from x-clipboard."
-  (interactive)
-  (let ((cli-cmd (cond ((eq system-type 'darwin) "pbpaste")
-                       ((eq system-type 'gnu/linux) "xsel --clipboard --input"))))
-   (if (display-graphic-p)
-      (progn
-        (clipboard-yank)
-        (message "graphics active"))
-     ;; TODO: check if the evil package is used
-     (if (eq evil-state 'normal)
-         (evil-append 1))
-    (insert (shell-command-to-string cli-cmd)))))
 
 (provide 'goodies)
