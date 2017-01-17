a24b6c337541fee0c6f5104d67146d6d365f229e
YiLiu6240 <yi.liu6240@gmail.com>
2017-01-17 21:18:37 +0000

    deprecate/rm packages

--- layers/yxl/packages.el
+++ layers/yxl/packages.el
@@ -1,6 +1,5 @@
 (setq yxl-packages '((yxl-utils :location site)
                      (goodies :location local)
-                     (alarm :location local)
                      (simple-todo :location local)
                      (scratch-pop :location local)
                      key-chord))
@@ -12,21 +11,13 @@
 (defun yxl/init-goodies ()
   (use-package goodies))
 
-(defun yxl/init-alarm ()
-  (use-package alarm
-    :init
-    (progn
-      (spacemacs/set-leader-keys "ota" #'alarm-clock)
-      (spacemacs/set-leader-keys "otc" #'alarm-countdown)
-      (spacemacs/set-leader-keys "otC" #'alarm-clock-cancel))))
-
-(defun yxl/init-simple-todo ()
-  (use-package simple-todo
-    :init
-    (progn
-      (spacemacs/set-leader-keys "ot1" #'yxl-set-simple-todo-task1)
-      (spacemacs/set-leader-keys "ot2" #'yxl-set-simple-todo-task2)
-      (spacemacs/set-leader-keys "ot3" #'yxl-set-simple-todo-task3))))
+;; (defun yxl/init-simple-todo ()
+;;   (use-package simple-todo
+;;     :init
+;;     (progn
+;;       (spacemacs/set-leader-keys "ot1" #'yxl-set-simple-todo-task1)
+;;       (spacemacs/set-leader-keys "ot2" #'yxl-set-simple-todo-task2)
+;;       (spacemacs/set-leader-keys "ot3" #'yxl-set-simple-todo-task3))))
 
 (defun yxl/init-scratch-pop ()
   (use-package scratch-pop
