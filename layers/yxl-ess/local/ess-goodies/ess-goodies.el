458256bb8ac132ae0d65be5824071da4dc90cbb0
YiLiu6240 <yi.liu6240@gmail.com>
2017-01-18 10:15:21 +0000

    add funcs; add sources

--- layers/yxl-ess/local/ess-goodies/ess-goodies.el
+++ layers/yxl-ess/local/ess-goodies/ess-goodies.el
@@ -1,6 +1,9 @@
-;; TODO: require ess? which ess?
 (require 'popup)
 (require 'ess-site)
+
+
+
+;; http://akhilsbehl.github.io/blog/2016/05/30/inspecting-objects-at-point-with-ess/
 
 (defun asb-read-into-string (buffer)
   (with-current-buffer buffer
@@ -29,5 +32,35 @@ Run R-FUN for object at point, and display results in a popup."
 (defun asb-ess-R-object-popup-interactive (r-func)
   (interactive "sR function to execute: ")
   (asb-ess-R-object-popup r-func))
+
+
+;; https://github.com/emacs-ess/ESS/pull/391
+
+(defun ess-beginning-of-pipe-or-end-of-line ()
+  "Find point position of end of line or beginning of pipe %>%"
+  (if (search-forward "%>%" (line-end-position) t)
+      (let ((pos (progn
+                   (beginning-of-line)
+                   (search-forward "%>%" (line-end-position))
+                   (backward-char 3)
+                   (point))))
+        (goto-char pos))
+    (end-of-line)))
+
+(defun ess-eval-pipe-through-line (vis)
+  "Like `ess-eval-paragraph' but only evaluates up to the pipe on this line.
+If no pipe, evaluate paragraph through the end of current line.
+Prefix arg VIS toggles visibility of ess-code as for `ess-eval-region'."
+  (interactive "P")
+  (save-excursion
+    (let ((end (progn
+                 (ess-beginning-of-pipe-or-end-of-line)
+                 (point)))
+          (beg (progn (backward-paragraph)
+                      (ess-skip-blanks-forward 'multiline)
+                      (point))))
+      (ess-eval-region beg end vis))))
+
+
 
 (provide 'ess-goodies)
