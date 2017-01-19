75c54e4462cec3fe13312e83c16c43e31a5bac19
YiLiu6240 <yi.liu6240@gmail.com>
2017-01-19 22:27:18 +0000

    mv path

--- layers/yxl-workspace/config.el
+++ layers/yxl-workspace/config.el
@@ -1,7 +1,7 @@
 ;; minimal dekstop setup, from emacswiki
 
 ;; use only one desktop
-(setq yxl-desktop-dirname "~/Dropbox/Inbox/")
+(setq yxl-desktop-dirname "~/Dropbox/inbox/")
 (setq desktop-base-file-name "yxl-emacs-desktop")
 (setq desktop-file-name-format 'tilde)
 (setq history-length 100)
