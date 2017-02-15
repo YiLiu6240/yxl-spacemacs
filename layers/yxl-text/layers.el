fbfefcf100f66a762d8b7e7f71ae930fddb4a19a
YiLiu6240 <yi.liu6240@gmail.com>
2017-02-15 11:34:56 +0000

    defualt latex build cmd to LatexMk

--- layers/yxl-text/layers.el
+++ layers/yxl-text/layers.el
@@ -3,10 +3,7 @@
    ;; markdown: need vmd from npm: npm install -g vmd
    pandoc
    (latex :variables
-          ;; LatexMK has to be enabled to let spc fetch auctex-latexmk
-          ;; latex-build-command "LatexMk"
-          ;; NOTE: could not get latexmk to work properly, use latex
-          latex-build-command "LaTeX"
+          latex-build-command "LatexMk"
           latex-enable-auto-fill nil
           latex-enable-folding t)
    bibtex))
