feffc7ba75b1944054a6e9ab768fada6901457fb
YiLiu6240 <yi.liu6240@gmail.com>
2017-02-15 11:41:05 +0000

    change auctex autogen folder names

--- layers/yxl-text/funcs.el
+++ layers/yxl-text/funcs.el
@@ -64,6 +64,8 @@
   (add-hook 'LaTeX-mode-hook (lambda () (setq line-spacing 4)))
   (add-hook 'LaTeX-mode-hook (lambda () (setq yxl-line-width 100)))
   (add-hook 'LaTeX-mode-hook (lambda () (auto-fill-mode -1)) t)
+  (setq TeX-auto-local ".auto")
+  (setq TeX-style-local ".style")
   (setq-default font-latex-fontify-script nil)
   ;; RET to newline no indent; S-RET to newline post indent
   (define-key LaTeX-mode-map (kbd "RET") 'electric-indent-just-newline)
