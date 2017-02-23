db6b6079fbc52e55b06c5a22f773997e3f41cabc
YiLiu6240 <yi.liu6240@gmail.com>
2017-02-23 11:30:49 +0000

    put hydras in a sep. file

--- init.el
+++ init.el
@@ -212,5 +212,6 @@
 (defun dotspacemacs/user-config ()
   (load-file (concat dotspacemacs-directory "config/config-post-init.el"))
   (load-file (concat dotspacemacs-directory "config/yxl-bindings.el"))
+  (load-file (concat dotspacemacs-directory "config/yxl-hydra.el"))
   (load-file (concat dotspacemacs-directory "config/hack-post-init.el"))
   (load-file (concat dotspacemacs-directory "config/config-last.el")))
