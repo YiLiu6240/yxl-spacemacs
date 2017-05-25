8787b81f2e777116dfe7ee95c5538158ca77d5fd
YiLiu6240 <yi.liu6240@gmail.com>
2017-05-25 09:56:06 +0100

    rm config re. python imenu
    There is a bug that prevents nested functions from showing

--- layers/yxl-prog/packages.el
+++ layers/yxl-prog/packages.el
@@ -22,28 +22,28 @@
                   (setq-local tab-width 4))))))
 
 (defun yxl-prog/post-init-python ()
-  (with-eval-after-load 'python)
+  (with-eval-after-load 'python))
   ;; (add-hook 'python-mode-hook 'evil-visual-mark-mode)
   ;; (add-hook 'python-mode-hook (lambda () (modify-syntax-entry ?_ "w")))
 
   ;; python imenu hack ----
   ;; http://stackoverflow.com/questions/21644876/imenu-does-not-work-for-python-mode-or-c-mode
-  (with-eval-after-load 'python
-    (defvar yxl-python-imenu-expression
-      '(("Class" "^class \\(.+\\):$" 1)
-        ("Function" "^def \\(.+\\)\(" 1)
-        ("Outline" "^\\(## .+\\)$" 1)
-        ("Outline" "^\\(### .+\\)$" 1)))
+  ;; (with-eval-after-load 'python
+  ;;   (defvar yxl-python-imenu-expression
+  ;;     '(("Class" "^class \\(.+\\):$" 1)
+  ;;       ("Function" "^def \\(.+\\)\(" 1)
+  ;;       ("Outline" "^\\(## .+\\)$" 1)
+  ;;       ("Outline" "^\\(### .+\\)$" 1)))
 
-    (defun yxl-python-imenu ()
-      "set python imenu items to customized item list"
-      (interactive)
-      (imenu--generic-function yxl-python-imenu-expression))
+  ;;   (defun yxl-python-imenu ()
+  ;;     "set python imenu items to customized item list"
+  ;;     (interactive)
+  ;;     (imenu--generic-function yxl-python-imenu-expression))
 
-    (add-hook
-     'python-mode-hook
-     (lambda ()
-       (setq imenu-create-index-function 'yxl-python-imenu)))))
+    ;; (add-hook
+    ;;  'python-mode-hook
+    ;;  (lambda ()
+    ;;    (setq imenu-create-index-function 'yxl-python-imenu)))))
 
 (defun yxl-prog/post-init-cc-mode ()
   (with-eval-after-load 'cc-mode))
