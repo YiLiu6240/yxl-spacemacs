9124fd09b2164cb64bb0eece14832f0165357ab7
YiLiu6240 <yi.liu6240@gmail.com>
2017-06-25 10:28:10 +0100

    wrap config in a dedicated variable

--- layers/yxl-datascience/packages.el
+++ layers/yxl-datascience/packages.el
@@ -40,31 +40,8 @@
     :config
     (progn
       (setq yxl-dp-docs
-            (delete-dups
-             (sort (append yxl-dp-docs
-                           '(("R - quantreg" . "https://cran.r-project.org/web/packages/quantreg/quantreg.pdf")
-                             ("Moab" . "http://docs.adaptivecomputing.com/torque/6-1-1/adminGuide/help.htm#topics/moabWorkloadManager/topics/moabCommands/a.gcommandoverview.html")
-                             ("Slurm" . "https://slurm.schedmd.com/")
-                             ("Cpp - Armadillo" . "http://arma.sourceforge.net/docs.html")
-                             ("Python - strftime" . "http://strftime.org/")
-                             ("Python - string format" . "https://pyformat.info/")
-                             ("GFM" . "https://guides.github.com/features/mastering-markdown/")
-                             ("R - adv-r" . "http://adv-r.had.co.nz/")
-                             ("R - rmarkdown - html" . "http://rmarkdown.rstudio.com/html_document_format.html")
-                             ("R - rmarkdown - cheatsheet" . "https://www.rstudio.com/wp-content/uploads/2015/03/rmarkdown-reference.pdf")
-                             ("R - knitr - options" . "https://yihui.name/knitr/options/")
-                             ("R - bookdown" . "https://bookdown.org/yihui/bookdown/")
-                             ("R - blogdown" . "https://bookdown.org/yihui/blogdown/")
-                             ("R - igraph" . "http://igraph.org/r/doc/00Index.html")
-                             ("R - cowplot" . "https://cran.r-project.org/web/packages/cowplot/")
-                             ("R - modelr" . "https://cran.r-project.org/web/packages/modelr/modelr.pdf")
-                             ("R - tidyverse - dplyr" . "http://dplyr.tidyverse.org/reference/index.html")
-                             ("R - tidyverse - stringr" . "http://stringr.tidyverse.org/reference/index.html")
-                             ("R - tidyverse - purrr" . "http://purrr.tidyverse.org/reference/index.html")
-                             ("Color - solarized" . "http://ethanschoonover.com/solarized")
-                             ("Color - gruvbox" . "https://github.com/morhetz/gruvbox")
-                             ("Color - material" . "https://material.io/guidelines/style/color.html#color-color-palette")
-                             ("Color - hexrgb" . "https://www.webpagefx.com/web-design/hex-to-rgb/")))
+            (delete-dups (sort (append yxl-dp-docs
+                                       yxl-datascience-additional-docs)
                    (lambda (elem1 elem2)
                      (let ((str1 (car elem1))
                            (str2 (car elem2)))
