8ff90ba8b681c8d2a345d285f6e67df2674d3466
YiLiu6240 <yi.liu6240@gmail.com>
2017-06-30 20:09:05 +0100

    add binding to insert pipe following RStudio convention

--- layers/yxl-datascience/funcs.el
+++ layers/yxl-datascience/funcs.el
@@ -48,6 +48,7 @@
   (define-key ess-mode-map (kbd "C-c C-.") #'yxl-datascience/insert-pipe)
   (define-key ess-mode-map (kbd "C-<tab>") #'sp-indent-adjust-sexp)
   (define-key ess-mode-map (kbd "C-S-<tab>") #'sp-dedent-adjust-sexp)
+  (define-key ess-mode-map (kbd "C-S-M") (lambda () (interactive) (insert "%>%")))
   (evil-define-key 'normal inferior-ess-mode-map
     (kbd "C-d") #'evil-scroll-down)
   (define-key ess-mode-map (kbd "C-,") #'ess-eval-region-or-line-and-step))
