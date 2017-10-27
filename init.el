6a79f8a763a9e1330b7c758c5f05f10ca8fc879b
YiLiu6240 <yi.liu6240@gmail.com>
2017-10-27 22:56:53 +0100

    Change startup banner

--- init.el
+++ init.el
@@ -132,8 +132,9 @@
    dotspacemacs-elpa-subdirectory nil
    dotspacemacs-editing-style 'vim
    dotspacemacs-verbose-loading nil
-   dotspacemacs-startup-banner (concat dotspacemacs-directory
-                                       "assets/emacs-china-icon-small.png")
+   ;; dotspacemacs-startup-banner (concat dotspacemacs-directory
+   ;;                                     "assets/emacs-china-icon-small.png")
+   dotspacemacs-startup-banner 0
    dotspacemacs-startup-lists nil
    dotspacemacs-startup-buffer-responsive t
    dotspacemacs-scratch-mode 'fundamental-mode
