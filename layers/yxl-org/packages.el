d97f022294ce2f13a865421ae3f8f83bbc80610f
YiLiu6240 <yi.liu6240@gmail.com>
2017-12-05 22:20:48 +0000

    Add package org-recipes

--- layers/yxl-org/packages.el
+++ layers/yxl-org/packages.el
@@ -2,7 +2,8 @@
                          (yxl-org :location site)
                          (org-display-inline-images-with-background :location site)
                          org-gcal
-                         ivy-todo))
+                         ivy-todo
+                         (org-recipes :location site)))
 
 (defun yxl-org/post-init-org ()
   ;; misc settings
@@ -67,3 +68,41 @@
       (ivy-add-actions
        'ivy-todo
        '(("v" ivy-todo-visit "visit"))))))
+
+(defun yxl-org/init-org-recipes ()
+  (use-package org-recipes
+    :defer t
+    :commands (org-recipes org-recipes-dwim)
+    :init
+    (progn
+      (spacemacs/set-leader-keys "aor" #'org-recipes))
+    :config
+    (progn
+      (require 'helm)
+      (setq org-recipes-file-list
+            (directory-files (expand-file-name (concat yxl-path-org
+                                                       "recipes/"))
+                             t "^.+\\.org"))
+      (defun org-recipes-goto-recipe ()
+        (interactive)
+        (with-helm-alive-p
+          (helm-exit-and-execute-action #'org-recipes--persistent-view)))
+      (defun org-recipes-goto-recipe-below ()
+        (interactive)
+        (let ((f (lambda (c)
+                   (split-window-below-and-focus)
+                   (find-file (org-recipes--get-file c))
+                   (goto-line (org-recipes--get-line c))
+                   (org-show-subtree))))
+          (with-helm-alive-p
+            (helm-exit-and-execute-action f))))
+      (define-key org-recipes-map
+        (kbd "C-u") #'org-recipes-goto-recipe)
+      (define-key org-recipes-map
+        (kbd "C-o") #'org-recipes-goto-recipe-below)
+      (define-key org-recipes-map
+        (kbd "RET") #'org-recipes-insert)
+      ;; HACK workaround a bug in org-recipes
+      (setq org-wiki-location
+            (expand-file-name (concat yxl-path-org
+                                      "recipes/"))))))
