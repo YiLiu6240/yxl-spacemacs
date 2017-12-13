8114ed99d4f2a801232b2f187fb4b5fa6d72e945
YiLiu6240 <yi.liu6240@gmail.com>
2017-12-13 17:36:57 +0000

    Increase large file limit for elfeed

--- init.el
+++ init.el
@@ -179,7 +179,7 @@
    dotspacemacs-default-layout-name "Default"
    dotspacemacs-display-default-layout nil
    dotspacemacs-auto-resume-layouts nil
-   dotspacemacs-large-file-size 3
+   dotspacemacs-large-file-size 100
    dotspacemacs-auto-save-file-location 'cache
    dotspacemacs-max-rollback-slots 5
    dotspacemacs-helm-resize nil
