59f3a51da3538042d457479ab6c972e4c2abecdc
YiLiu6240 <yi.liu6240@gmail.com>
2017-12-30 21:32:55 +0800

    Refine export-agenda: respect user agenda configs instead of hardcoding

--- layers/yxl-org/local/org-goodies/org-goodies.el
+++ layers/yxl-org/local/org-goodies/org-goodies.el
@@ -4,23 +4,24 @@
 (defvar org-goodies-agenda-file "~/.org-agenda"
   "File to which we export agenda")
 
-(defun org-goodies-export-agenda ()
+(defun org-goodies-export-agenda (&optional agenda-file agenda-args)
   "
+Export agenda to a file.
+
 Source: https://www.reddit.com/r/emacs/comments/7mec6e/orgmode_view_agenda_in_desktop_without_opening/"
   (interactive)
-  (let* ((org-agenda-span 'day)
-         (org-agenda-use-time-grid t)
-         (org-agenda-remove-tags t)
-         (org-agenda-window-setup 'nope)
+  (let* ((org-agenda-window-setup 'nope)
          (wins (current-window-configuration))
-         (org-agenda-sticky nil))
+         (org-agenda-sticky nil)
+         (file (or agenda-file org-goodies-agenda-file))
+         (args (or agenda-args '(nil "a"))))
     (save-excursion
       (with-current-buffer
           (get-buffer-create org-agenda-buffer-name)
         (pop-to-buffer (current-buffer))
-        (org-agenda nil "a")
+        (apply #'org-agenda args)
         (let ((result (buffer-string)))
-          (with-temp-file org-goodies-agenda-file (insert result)))))
+          (with-temp-file file (insert result)))))
     (set-window-configuration wins)))
 
 (provide 'org-goodies)
