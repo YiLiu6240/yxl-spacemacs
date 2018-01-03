b607a997a3469832db01431a382882277ba5f0ab
YiLiu6240 <yi.liu6240@gmail.com>
2018-01-04 07:48:52 +0800

    Hide mu4e indexing message

--- layers/yxl-email/packages.el
+++ layers/yxl-email/packages.el
@@ -25,6 +25,7 @@
       (yxl-email/mu4e-setup-general-keybindings)
       (yxl-email/mu4e-setup-evilified-keybindings)
       (yxl-email/mu4e-setup-leader-keys)
+      (setq mu4e-hide-index-messages t)
       (setq mu4e-completing-read-function 'completing-read)
       (setq mu4e-update-interval nil)
       (setq mu4e-compose-signature-auto-include nil)
