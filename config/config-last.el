51cf34b949f1db5a84a13730830b703f11006328
YiLiu6240 <yi.liu6240@gmail.com>
2018-03-04 14:08:09 +0000

    Simplify org dependency; Abstain from org's task system

--- config/config-last.el
+++ config/config-last.el
@@ -4,4 +4,16 @@
               (interactive)
               (find-file "~/Downloads/")))
 
-(org-agenda nil "A")
+;; NOTE: Frustrated with org's task system
+;; (org-agenda nil "A")
+(require 'org)
+;; HACK:
+;;   Speed up loading of org-capture
+;;
+;;   something is wrong with several packages
+;;   loaded despite of use-package
+;;   when org-capture is called the first time.
+;;
+;;   Need to review use-package following
+;;   Spacemacs' dev update
+(call-interactively 'org-store-link)
