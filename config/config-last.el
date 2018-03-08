3905a62a21a82f23b2c3e080e93e27b346a0ef4c
YiLiu6240 <yi.liu6240@gmail.com>
2018-03-08 16:28:41 +0000

    Open yxl-file-org-checkbox at the end of the loading process

--- config/config-last.el
+++ config/config-last.el
@@ -4,16 +4,5 @@
               (interactive)
               (find-file "~/Downloads/")))
 
-;; NOTE: Frustrated with org's task system
-;; (org-agenda nil "A")
-(require 'org)
-;; HACK:
-;;   Speed up loading of org-capture
-;;
-;;   something is wrong with several packages
-;;   loaded despite of use-package
-;;   when org-capture is called the first time.
-;;
-;;   Need to review use-package following
-;;   Spacemacs' dev update
-(call-interactively 'org-store-link)
+;; Simply load the most frequently used org file
+(find-file yxl-file-org-checkbox)
