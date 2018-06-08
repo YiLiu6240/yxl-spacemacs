f1682452668774c5779a1c5eb26368f207db0b9b
YiLiu6240 <yi.liu6240@gmail.com>
2018-06-08 09:38:46 +0100

    Do not show magit status in Windows

--- theme/yxl-airline/yxl-airline.el
+++ theme/yxl-airline/yxl-airline.el
@@ -129,7 +129,7 @@
                           (lhs-rest (list
                                      (powerline-raw " " center-face 'lr)
                                      ;; magit
-                                     (when (featurep 'magit)
+                                     (when (and (not (spacemacs/system-is-mswindows)) (featurep 'magit))
                                        (powerline-raw (concat pl-sep (magit-get-current-branch) pl-sep) center-face 'lr))
                                      ;; selection info
                                      (when (or mark-active
