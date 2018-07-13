09539364726cff4fb49e4bcf21f5d7bd3c796cbf
YiLiu6240 <yi.liu6240@gmail.com>
2018-07-13 19:28:42 +0100

    Update configs in email layer

--- layers/+webservice/yxl-email/packages.el
+++ layers/+webservice/yxl-email/packages.el
@@ -16,28 +16,27 @@
     (progn
       (spacemacs/declare-prefix "am" "email")
       (spacemacs/set-leader-keys
-        "a m m" #'mu4e
-        "a m u" #'mu4e-update-mail-and-index
-        "a m c" #'mu4e-compose-new
-        "a m q" #'yxl-email/mu4e-offlineimap-quiet
-        "a m Q" #'yxl-email/mu4e-offlineimap-quick))
+        "amm" #'mu4e
+        "amu" #'mu4e-update-mail-and-index
+        "amc" #'mu4e-compose-new
+        "amq" #'yxl-email/mu4e-offlineimap-quiet
+        "amQ" #'yxl-email/mu4e-offlineimap-quick))
     :config
     (progn
       (yxl-email/mu4e-setup-general-keybindings)
       (yxl-email/mu4e-setup-evilified-keybindings)
       (yxl-email/mu4e-setup-leader-keys)
+      (setq mu4e-get-mail-command "offlineimap")
+      ;; ---- UI ----
       (setq mu4e-headers-visible-columns 100)
       (setq mu4e-headers-visible-lines 15)
-      (setq mu4e-hide-index-messages t)
-      (setq mu4e-completing-read-function 'completing-read)
-      (setq mu4e-update-interval nil)
-      (setq mu4e-compose-signature-auto-include nil)
+      (setq mu4e-headers-show-threads nil)
       (setq mu4e-view-show-images t)
       (setq mu4e-view-show-addresses t)
-      (setq message-kill-buffer-on-exit t)
-      (setq mu4e-context-policy 'pick-first)
-      (setq mu4e-confirm-quit nil)
+      (setq mu4e-hide-index-messages t)
       (setq mu4e-enable-notifications t)
+      (setq mu4e-confirm-quit nil)
+      (setq message-kill-buffer-on-exit t)
       (setq mu4e-headers-date-format "%a %Y-%m-%d %H:%M")
       (setq mu4e-headers-fields
             '((:human-date . 20)
@@ -59,6 +58,16 @@
                               (concat "maildir:" (car maildir)))
                             mu4e-maildir-shortcuts) " OR ")
                "All inboxes" ?i)))
+      ;; ---- indexing ----
+      ;; don't do a full cleanup check
+      (setq mu4e-index-cleanup nil)
+      ;; don't consider up-to-date dirs
+      (setq mu4e-index-lazy-check t)
+      ;; ---- others ----
+      (setq mu4e-completing-read-function 'completing-read)
+      (setq mu4e-update-interval 900)
+      (setq mu4e-compose-signature-auto-include nil)
+      (setq mu4e-context-policy 'pick-first)
       (add-hook 'mu4e-compose-mode-hook (lambda () (auto-fill-mode -1)))
       (advice-add #'mu4e-headers-view-message :around
                   #'mu4e-split-view-dynamically)
@@ -70,8 +79,7 @@
       (yxl-email/mu4e-set-html2text-command-to-shr2text)
       (setq shr-color-visible-luminance-min 90)
       (setq shr-color-visible-distance-min 20)
-      (setq mu4e-completing-read-function #'ivy-completing-read)
-      (setq mu4e-headers-show-threads nil))))
+      (setq mu4e-completing-read-function #'ivy-completing-read))))
 
 (defun yxl-email/init-mu4e-alert ()
   (use-package mu4e-alert
